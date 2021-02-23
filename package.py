768fec988a744024a371476faa46a43cb9eea917
Naomi Que <149199@ocelot.ala.int.uts.edu.au>
2021-02-23 15:50:48 +1100

    Added package and CMakeLists to build tk_multi_breakdown

---
+++ package.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+name = 'tk_multi_breakdown'
+
+version = '1.6.0.0'
+
+requires = []
+
+description = \
+    """ 
+    utsala fork of shotgunsoftware/tk-multi-breakdown
+    """
+
+
+def commands():
+    env.TK_MULTI_BREAKDOWN_BUNDLE = '{this.root}'
+
+uuid = '49ca090b5f46462e8f5be808f205e0e5'
